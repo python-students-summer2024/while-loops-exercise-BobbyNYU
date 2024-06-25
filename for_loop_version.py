30a38cc31f20bd26481ff04a0ea2cb1c32c0de48
BobbyNYU <bsn2012@nyu.edu>
2024-06-24 21:25:17 -0500

    assignment complete!

---
+++ for_loop_version.py
@@ -0,0 +1,23 @@
+def get_starting_number():
+    while True:
+        try:
+            num = int(input("How many bottles of beer on the wall? "))
+            if num >= 1:
+                return num
+            else:
+                print("Please enter an integer 1 or greater.")
+        except ValueError:
+            print("Please enter a valid integer.")
+
+def sing(starting_bottles):
+    for bottles in range(starting_bottles, 0, -1):
+        if bottles == 1:
+            print(f"1 bottle of beer on the wall, 1 bottle of beer!")
+            print("Take one down, pass it around, no more bottles of beer on the wall!\n")
+        else:
+            print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer!")
+            print(f"Take one down, pass it around, {bottles-1} {'bottle' if bottles-1 == 1 else 'bottles'} of beer on the wall!\n")
+
+if __name__ == "__main__":
+    starting_bottles = get_starting_number()
+    sing(starting_bottles)
---
+++ while_loop_flag_version.py
@@ -0,0 +1,27 @@
+def get_starting_number():
+    while True:
+        try:
+            num = int(input("How many bottles of beer on the wall? "))
+            if num >= 1:
+                return num
+            else:
+                print("Please enter an integer 1 or greater.")
+        except ValueError:
+            print("Please enter a valid integer.")
+
+def sing(starting_bottles):
+    bottles = starting_bottles
+    keep_singing = True
+    while keep_singing:
+        if bottles == 1:
+            print(f"1 bottle of beer on the wall, 1 bottle of beer!")
+            print("Take one down, pass it around, no more bottles of beer on the wall!\n")
+            keep_singing = False
+        else:
+            print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer!")
+            print(f"Take one down, pass it around, {bottles-1} {'bottle' if bottles-1 == 1 else 'bottles'} of beer on the wall!\n")
+            bottles -= 1
+
+if __name__ == "__main__":
+    starting_bottles = get_starting_number()
+    sing(starting_bottles)
